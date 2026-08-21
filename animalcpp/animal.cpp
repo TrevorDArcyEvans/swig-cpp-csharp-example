65dfd17c7f8e977aafe4385ae43c179abdc6df91
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-08-21 13:15:31 +0100

    - formatting

--- animalcpp/animal.cpp
+++ animalcpp/animal.cpp
@@ -3,15 +3,15 @@
 
 Animal::Animal(std::string _name)
 {
-	name = _name;
+    name = _name;
 }
 
 void Animal::Walk()
 {
-	std::cout << name << " is walking..." << '\n';
+    std::cout << name << " is walking..." << '\n';
 }
 
 std::string& Animal::GetName()
 {
-	return name;
+    return name;
 }
